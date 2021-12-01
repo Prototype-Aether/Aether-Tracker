78531dbda49f5ff017983c84bc1275d50a93ccd3
HotMonkeyWings <devsony52@gmail.com>
2021-12-01 18:17:29 +0530

    Added clones

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,6 @@
 use aether_lib::tracker::TrackerPacket;
 use itertools::Itertools;
-use netfunc::{identity_report, identity_request, connection_poll, PeerInfo};
+use netfunc::{connection_poll, identity_report, identity_request, connection_request, PeerInfo};
 use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::env;
@@ -11,27 +11,36 @@ use std::{thread, time};
 
 struct PeerModel {
     socket: UdpSocket,
+    username: String,
     peer: HashMap<String, PeerInfo>,
-    requests: HashMap<String, u32>
+    requests: Vec<String>,
+    // [0] => identity = 1 => peer = B
+    // [1] => identity = 2 => peer = C
 }
 
 impl PeerModel {
-    pub fn _new(host_addr: String) -> Self {
+    pub fn _new(username: String, host_addr: String) -> Self {
         PeerModel {
             socket: UdpSocket::bind(host_addr).unwrap(),
+            username: username,
             peer: HashMap::new(),
-            requests: Vec::new()
+            requests: Vec::new(),
         }
     }
 
     pub fn keepalive(&self, username: String, tracker_addr: String, identity_number: u32) {
         loop {
-            connection_poll(username.clone(), identity_number, &self.socket, tracker_addr.clone());
+            connection_poll(
+                username.clone(),
+                identity_number, // Not necessary
+                &self.socket,
+                tracker_addr.clone(),
+            );
             thread::sleep(time::Duration::from_secs_f32(2.0));
         }
     }
 
-    pub fn listener(&self) {
+    pub fn listener(&mut self, tracker_addr: String) {
         loop {
             let mut buffer = [0; 2048];
             let (amt, _src) = self.socket.recv_from(&mut buffer).unwrap();
@@ -39,7 +48,6 @@ impl PeerModel {
 
             if !data.req {
                 match Option::from(data.packet_type) {
-
                     // Process Response to Identity Request
                     Some(1) => {
                         // Establish connection here
@@ -64,31 +72,38 @@ impl PeerModel {
 
                     // Process response to Connection Poll
                     Some(3) => {
-                        if data.connections.is_empty(){
-                        }
-                        else {
-                            for connection in data.connections.iter(){
-                                match self.requests.get(&connection.username){
-                                    None => {
-                                        // Spawn a new connection request
-                                    }
-                                    Some(conn) => {
-                                        let packet: TrackerPacket = TrackerPacket {
-                                            identity_number: data.identity_number,
-                                            peer_username: "".to_string(),
-                                            connections: Vec::new(),
-                                            username: "Hey we did it!".to_string(),
-                                            req: false,
-                                            packet_type: 5 as u8,
-                                            port: 0000,
-                                            ip: [0, 0, 0, 0],
-                                        };
-                                        let buffer: Vec<u8> = TryFrom::try_from(packet).unwrap();
-                                        let addr = format!("{}:{}", connection.ip.iter().join("."), connection.port);
-                                        (self.socket)
-                                            .send_to(&buffer, addr)
-                                            .expect("Not sent");
-                                    }
+                        if !data.connections.is_empty() {
+                            for connection in data.connections.iter() {
+                                // match self.requests.iter().position(|&user| user == data.username))
+                                // If A has already sent a connection Request
+                                if self.requests.contains(&connection.username)
+                                {
+                                    // Handshake occurs from here
+                                    let index: u32 = self.requests.iter().position(|&user| user == data.username).unwrap() as u32 + 1;
+                                    let packet: TrackerPacket = TrackerPacket {
+                                        identity_number: index,
+                                        peer_username: "".to_string(),
+                                        connections: Vec::new(),
+                                        username: "Hey we did it!".to_string(),
+                                        req: false,
+                                        packet_type: 5 as u8,
+                                        port: 0000,
+                                        ip: [0, 0, 0, 0],
+                                    };
+                                    let buffer: Vec<u8> = TryFrom::try_from(packet).unwrap();
+                                    let addr = format!(
+                                        "{}:{}",
+                                        connection.ip.iter().join("."),
+                                        connection.port
+                                    );
+                                    (self.socket).send_to(&buffer, addr).expect("Not sent");
+                                    
+                                }
+
+                                // A spawns a connection request
+                                else {
+                                    self.requests.push(connection.username.clone());
+                                    connection_request(self.username.clone(), data.identity_number + 1, connection.username.clone(), &self.socket, tracker_addr.clone());
                                 }
                             }
                         }
@@ -110,18 +125,15 @@ struct Peer {
 }
 
 impl Peer {
-    pub fn new(recv_addr: String) -> Self {
+    pub fn new(username:String, recv_addr: String) -> Self {
         Peer {
-            model: Arc::new(PeerModel::_new(recv_addr)),
+            model: Arc::new(PeerModel::_new(username, recv_addr)),
         }
     }
 
     pub fn start(&self, tracker_addr: String) {
         // Report identity to tracker
-        let mut username = String::new();
-        io::stdin()
-            .read_line(&mut username)
-            .expect("Failed to read message");
+        
 
         let mut buffer = [0; 2048];
         self.model
@@ -132,7 +144,7 @@ impl Peer {
         // Loop until reply is received.
         loop {
             identity_report(
-                String::from(username.trim()),
+                String::from(self.model.username.trim()),
                 &self.model.socket,
                 &tracker_addr,
                 false,
@@ -165,7 +177,7 @@ impl Peer {
         // });
 
         thread::spawn(move || {
-            listener_thread.listener();
+            listener_thread.listener(tracker_addr);
         });
         let mut peer = String::new();
         println!("Enter peer to connect: ");
@@ -196,6 +208,11 @@ fn main() {
 
     let host_addr = format!("0.0.0.0:{}", args[1]);
     let tracker_addr = args[2].clone();
-    let peer = Peer::new(host_addr);
+    println!("Enter username: ");
+    let mut username = String::new();
+        io::stdin()
+            .read_line(&mut username)
+            .expect("Failed to read message");
+    let peer = Peer::new(username, host_addr);
     peer.start(tracker_addr);
 }
